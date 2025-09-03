3ff02f0f2a4d0c30dccadbabbec347e86c8be395
kevinVtt <kevinvittor579@gmail.com>
2025-09-03 15:22:57 -0300

    add logica en ventana principal

--- app/src/main/java/com/example/tp_seminarios/Principal.kt
+++ app/src/main/java/com/example/tp_seminarios/Principal.kt
@@ -1,12 +1,43 @@
 package com.example.tp_seminarios
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Principal : AppCompatActivity() {
+
+    lateinit var toolbar : Toolbar
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_principal,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        lateinit var intent : Intent
+        return when (item.itemId){
+            R.id.lista_pokemones -> {
+                intent = Intent(this, ListaPokemones::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.cerrar_session -> {
+                intent = Intent(this, Login::class.java)
+                startActivity(intent)
+                finish()
+                true
+            }
+            else -> return super.onOptionsItemSelected(item)
+
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +47,10 @@ class Principal : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = getString(R.string.barra_principal)
+
     }
 }
