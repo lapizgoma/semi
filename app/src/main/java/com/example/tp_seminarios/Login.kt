986c69745c586c9918dd5753e4cca16c7f36014e
kevinVtt <kevinvittor579@gmail.com>
2025-09-03 15:22:27 -0300

    add restricciones

--- app/src/main/java/com/example/tp_seminarios/Login.kt
+++ app/src/main/java/com/example/tp_seminarios/Login.kt
@@ -2,15 +2,24 @@ package com.example.tp_seminarios
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.io.path.Path
 
 class Login : AppCompatActivity() {
 
-    lateinit var tvOlvidoPassword: TextView
+    private lateinit var tvOlvidoPassword: TextView
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var cbCheckPassword: CheckBox
+    private lateinit var btnIniciarSession: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +31,12 @@ class Login : AppCompatActivity() {
             insets
         }
         tvOlvidoPassword = findViewById(R.id.olvidoPassword);
+        btnIniciarSession = findViewById(R.id.btnIniciarSession)
+        etEmail = findViewById(R.id.inputEmail)
+        etPassword = findViewById(R.id.inputPassword)
+        cbCheckPassword = findViewById(R.id.cbRecordarPassword)
         handlerOlvidoPassword(tvOlvidoPassword)
+        iniciarSession(btnIniciarSession)
 
     }
 
@@ -31,6 +45,37 @@ class Login : AppCompatActivity() {
             val intent = Intent(this, ForgetPassword::class.java)
             startActivity(intent)
             finish()
+        }
+    }
+
+    private fun iniciarSession(btnIniciarSession: Button){
+
+        btnIniciarSession.setOnClickListener {
+            val email = etEmail.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+
+            when{
+                email.isEmpty() ->{
+                    etEmail.error = "El email no puede estar vacio"
+                    etEmail.requestFocus()
+                }
+                !email.contains("@") ->{
+                    etEmail.error = "El formato del email es errado"
+                    etEmail.requestFocus()
+                }
+                password.isEmpty() ->{
+                    etPassword.error = "La contraseña no puede estar vacia"
+                    etPassword.requestFocus()
+                }
+                else ->{
+                    if(cbCheckPassword.isChecked){
+                        Toast.makeText(this,"Usuario recordado", Toast.LENGTH_SHORT).show()
+                    }
+                    val intent = Intent(this, Principal::class.java)
+                    startActivity(intent)
+                }
+            }
+
         }
     }
 }
