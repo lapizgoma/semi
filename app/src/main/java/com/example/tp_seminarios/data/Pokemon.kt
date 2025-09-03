a95e1af8dfee20fac5e4c744a083efbc4f8401c9
kevinVtt <kevinvittor579@gmail.com>
2025-09-03 15:21:38 -0300

    add informacion pokemon

--- app/src/main/java/com/example/tp_seminarios/data/Pokemon.kt
+++ app/src/main/java/com/example/tp_seminarios/data/Pokemon.kt
@@ -1,4 +1,14 @@
 package com.example.tp_seminarios.data
 
-class Pokemon {
-}
+import java.io.Serializable
+// Hacemos que sea Serializable para que luego lo podamos usar con el Intent en casos futuros
+data class Pokemon(
+    val nombre: String,
+    val tipo: String,
+    val nivel: Int,
+    val hp: Int,
+    val ataque: Int,
+    val defensa: Int,
+    val velocidad: Int,
+    val descripcion: String
+) : Serializable
