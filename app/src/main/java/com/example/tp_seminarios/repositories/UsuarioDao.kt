492d776eefff543e494b71ef8ce8aa43c9ed2e5d
kevinVtt <kevinvittor579@gmail.com>
2025-09-13 05:07:26 -0300

    add repository dao

--- app/src/main/java/com/example/tp_seminarios/repositories/UsuarioDao.kt
+++ app/src/main/java/com/example/tp_seminarios/repositories/UsuarioDao.kt
@@ -1,4 +1,26 @@
 package com.example.tp_seminarios.repositories
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.tp_seminarios.data.Usuario
+
+@Dao
 interface UsuarioDao {
+    @Query("SELECT * FROM usuarios")
+    fun getAll(): List<Usuario>
+
+    @Query("SELECT * FROM usuarios as u WHERE u.id = :id")
+    fun getUsuarioPorId(id: Int): Usuario?
+
+    @Query("SELECT * FROM usuarios as u WHERE u.email = :email AND u.contrasenia = :password")
+    fun getUsuarioPorEmailYPassword(email: String, password: String): Usuario?
+
+    @Insert
+    fun insertUsuario(usuario: Usuario)
+
+    @Delete
+    fun deleteUsuario(usuario: Usuario)
+
 }
