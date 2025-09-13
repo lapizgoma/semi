c5810c440b423f3e6e5f42a8c9e8fc20c7c4bc97
kevinVtt <kevinvittor579@gmail.com>
2025-09-13 05:07:11 -0300

    add estructura basica del user

--- app/src/main/java/com/example/tp_seminarios/data/Usuario.kt
+++ app/src/main/java/com/example/tp_seminarios/data/Usuario.kt
@@ -1,3 +1,21 @@
 package com.example.tp_seminarios.data
 
-data class Usuario()
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDate
+
+@Entity(tableName = "usuarios")
+data class Usuario(
+    @ColumnInfo(name = "nombre_usuario")
+    var nombreUsuario: String,
+    @ColumnInfo(name = "contrasenia")
+    var password: String,
+    @ColumnInfo(name = "email")
+    var email: String
+){
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+    @ColumnInfo(name = "fecha_alta")
+    var fechaAlta: LocalDate = LocalDate.now()
+}
