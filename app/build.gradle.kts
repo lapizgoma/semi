50fce351b0667ab7da5b123cf105085a2f994054
kevinVtt <kevinvittor579@gmail.com>
2025-10-08 16:40:45 -0300

    update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,5 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
 }
